214220196e229d0912c83fbd16fc4dfb48b39a45
Apple <apple@Apples-MacBook-Pro-84.local>
2019-08-08 16:43:16 -0400

    Front end working

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func showMessages(sender: UIButton){
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
